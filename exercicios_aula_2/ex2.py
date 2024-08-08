ff65a5c85f7b8b6f8934348bc7d3b64b2dfe3dbb
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-08-08 11:43:50 -0300

    [EX2] - Adicionando os exercicios para fazer.

---
+++ exercicios_aula_2/ex2.py
@@ -0,0 +1,38 @@
+#A ind ́ustria IFAC produz 2 tipos de produtos: P1 e P2. O lucro
+#por unidade de P1  ́e de $100 e o lucro unit ́ario de P2  ́e de $150. A empresa
+#necessita de 2 horas para fabricar uma unidade de P1 e 3 horas para fabricar
+#uma unidade de P2. O tempo mensal dispon ́ıvel para essas atividades  ́e de
+#120 horas. As demandas esperadas para os 2 produtos levaram a empresa
+#a decidir que os montantes produzidos de P1 e P2 n ̃ao devem ultrapassar
+#40 unidades de P1 e 30 unidades de P2 por mˆes. Construa o modelo do
+#sistema de produ ̧c ̃ao mensal com o objetivo de maximizar o lucro da empresa.
+#Forne ̧ca tamb ́em uma solu ̧c ̃ao vi ́avel e o custo associado a ela na
+#fun ̧c ̃ao objetivo.
+
+
+### Lucro de P1 -> 100
+### Lucro de P2 -> 150
+
+### Tempo de produção de P1 -> 2 horas
+### Tempo de produção de P2 -> 3 horas
+
+### Tempo mensal disponível -> 120 horas
+
+### MODELANDO O PROBLEMA
+# Variáveis de decisão
+# x1 -> quantidade de produtos P1
+# x2 -> quantidade de produtos P2
+
+# Função objetivo
+
+# f(max) = 100x1 + 150x2
+
+# Restrições
+# 2x1 + 3x2 <= 120
+# x1 <= 40
+# x2 <= 30
+
+
+
+
+
